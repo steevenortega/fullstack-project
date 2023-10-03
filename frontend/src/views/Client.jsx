64ecf0eb366fd1a3a358848dd61c675d7e4ffb79
steevenortega <steevenortega@gmail.com>
2023-10-02 19:39:41 -0500

    Se crean las vistas para obtener los campos de clientes y contratos

--- frontend/src/views/Client.jsx
+++ frontend/src/views/Client.jsx
@@ -1,12 +1,122 @@
-const Client = () => {
-    return (
-        <section>
-            <h1>Seccion de clientes</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam voluptates a praesentium beatae quaerat earum ab odio quibusdam minima cumque amet deserunt ut, fugiat molestias vitae sed dolorem eius!</p>
-        
-        </section>
-        
-    );
-};
+import React from 'react';
+import '../index.css';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Table } from "reactstrap"
 
-export default Client;
+
+
+function Client() {
+    const navigate = useNavigate()
+    const [clients, setClients] = useState([]);
+    const [modal, setModal] = useState(false)
+    const [aboutToDeleteClient, setAboutToDeleteClient] = useState({})
+
+
+    const getClients = () => {
+      axios.get('http://localhost:3000/clients/')
+      
+      
+      .then(response => {
+        console.log(response)
+          setClients(response.data)
+      })
+    }
+  
+    useEffect(() => {
+
+      getClients();
+  
+     
+      
+  }, []);
+
+  
+
+  const openModal = (client) => {
+    setModal(true)
+    setAboutToDeleteClient(client)
+  }
+
+  const handleDeleteClient = (id) => {
+    axios.delete(`http://localhost:3000/clients/${id}`)
+      .then(res => {
+        getClients();
+      })
+    setModal(false)
+  }
+  
+  return (
+    <>
+      <div className="container">
+      <div>
+        <Link
+          className="btn btn-success"
+          to="/client/create"
+        >
+          Añadir cliente
+        </Link>
+      </div>
+
+      <Table
+>
+  <thead>
+    <tr>
+      <th>
+        #
+      </th>
+      <th>
+        Nombre cliente
+      </th>      
+    </tr>
+  </thead>
+  <tbody>
+  {
+        clients?.map(client=>{return(
+        <tr key={client.id}>
+            <th>
+                {client.id}
+            </th>
+            <td>
+            {client.nombres}
+            </td> 
+            <td>  
+            <Link
+                    className="btn btn-primary"
+                    to={`/client/edit/${client.id}`}
+                  >
+                    Editar
+                  </Link>
+                  <Button onClick={() => openModal(client)}>Eliminar</Button>
+                </td>         
+            </tr>)           
+        })
+       }    
+  </tbody>
+</Table>
+      
+      </div>
+
+      <Modal isOpen={modal} toggle={() => setModal(!modal)}>
+        <ModalHeader toggle={() => setModal(!modal)}>Modal title</ModalHeader>
+        <ModalBody>
+          ¿Estas seguro de eliminar la empresa <span className="fw-bold"> {aboutToDeleteClient.nombres}</span>?
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => handleDeleteClient(aboutToDeleteClient.id)}>
+            Eliminar
+          </Button>{' '}          
+          <Button color="secondary" onClick={() => setModal(false)}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  )
+  }  
+
+  export default Client;
--- frontend/src/views/Contract.jsx
+++ frontend/src/views/Contract.jsx
@@ -1,12 +1,134 @@
-const Contract = () => {
-    return (
-        <section>
-            <h1>Mi empresa</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam voluptates a praesentium beatae quaerat earum ab odio quibusdam minima cumque amet deserunt ut, fugiat molestias vitae sed dolorem eius!</p>
-        
-        </section>
-        
-    );
-};
+import React from 'react';
+import '../index.css';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Table } from "reactstrap"
 
-export default Contract;
+
+
+function Contract() {
+    const navigate = useNavigate()
+    const [contracts, setContracts] = useState([]);
+    const [modal, setModal] = useState(false)
+    const [aboutToDeleteContract, setAboutToDeleteContract] = useState({})
+
+
+    const getContracts = () => {
+      axios.get('http://localhost:3000/contracts/')
+      
+      
+      .then(response => {
+        console.log(response)
+          setContracts(response.data)
+      })
+    }
+  
+    useEffect(() => {
+
+      getContracts();
+  
+     
+      
+  }, []);
+
+  
+
+  const openModal = (contract) => {
+    setModal(true)
+    setAboutToDeleteContract(contract)
+  }
+
+  const handleDeleteContract = (id) => {
+    axios.delete(`http://localhost:3000/contracts/${id}`)
+      .then(res => {
+        getContracts();
+      })
+    setModal(false)
+  }
+  
+  return (
+    <>
+      <div className="container">
+      <div>
+        <Link
+          className="btn btn-success"
+          to="/contract/create"
+        >
+          Añadir contrato
+        </Link>
+      </div>
+
+      <Table
+>
+  <thead>
+    <tr>
+      <th>
+        #
+      </th>
+      <th>
+        Fecha inicio
+      </th>  
+      <th>
+        Fecha fin
+      </th> 
+      <th>
+        Valor contrato
+      </th>     
+    </tr>
+  </thead>
+  <tbody>
+  {
+        contracts?.map(contract=>{return(
+        <tr key={contract.id}>
+            <th>
+                {contract.id}
+            </th>
+            <td>
+            {contract.fecha_inicio}
+            </td> 
+            <td>
+            {contract.fecha_fin}
+            </td> 
+            <td>
+            {contract.valor_contrato}
+            </td> 
+            <td>  
+            <Link
+                    className="btn btn-primary"
+                    to={`/contract/edit/${contract.id}`}
+                  >
+                    Editar
+                  </Link>
+                  <Button onClick={() => openModal(contract)}>Eliminar</Button>
+                </td>         
+            </tr>)           
+        })
+       }    
+  </tbody>
+</Table>
+      
+      </div>
+
+      <Modal isOpen={modal} toggle={() => setModal(!modal)}>
+        <ModalHeader toggle={() => setModal(!modal)}>Modal title</ModalHeader>
+        <ModalBody>
+          ¿Estas seguro de eliminar el contrato <span className="fw-bold"> {aboutToDeleteContract.id}</span>?
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={() => handleDeleteContract(aboutToDeleteContract.id)}>
+            Eliminar
+          </Button>{' '}          
+          <Button color="secondary" onClick={() => setModal(false)}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  )
+  }  
+
+  export default Contract;
